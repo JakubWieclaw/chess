ff5cf17a993cf1b9493d4692bfbdbe622b8500b9
JakubWieclaw <jwieclaw000@gmail.com>
2022-12-14 04:10:45 +0100

    add checking legality of move, add coments on what's next

--- source/engine/Board.cpp
+++ source/engine/Board.cpp
@@ -3,15 +3,15 @@
 
 Colour Board::colour_turn() // Whose turn is it - dumb, don't you think?
 {
-    return history.get_size()%2 == 0? Colour::White: Colour::Black;
+    return history.get_size() % 2 == 0 ? Colour::White : Colour::Black;
 }
 
 void Board::start(std::string path)
 {
     std::ifstream file(path);
-    for(int i = 0; i < 8; ++i)
+    for (int i = 0; i < 8; ++i)
     {
-        for(int j = 0; j < 8; ++j) 
+        for (int j = 0; j < 8; ++j)
         {
             chessboard[i][j].f.row = i;
             chessboard[i][j].f.col = j;
@@ -21,37 +21,36 @@ void Board::start(std::string path)
             {
                 c = file.get();
             }
-            chessboard[i][j].p.colour = i<4 ? Colour::White: Colour::Black;
+            chessboard[i][j].p.colour = i < 4 ? Colour::White : Colour::Black;
             switch (c)
             {
-                case 'r':
-                    chessboard[i][j].p.type = Piece_type::Rook;
-                    break;
-                case 'n':
-                    chessboard[i][j].p.type = Piece_type::Knight;
-                    break;
-                case 'b':
-                    chessboard[i][j].p.type = Piece_type::Bishop;
-                    break;
-                case 'q':
-                    chessboard[i][j].p.type = Piece_type::Queen;
-                    break;
-                case 'k':
-                    chessboard[i][j].p.type = Piece_type::King;
-                    break;
-                case 'p':
-                    chessboard[i][j].p.type = Piece_type::Pawn;
-                    break;
-                default:
-                    chessboard[i][j].p.type = Piece_type::None;
-                    chessboard[i][j].occupied = false;
+            case 'r':
+                chessboard[i][j].p.type = Piece_type::Rook;
+                break;
+            case 'n':
+                chessboard[i][j].p.type = Piece_type::Knight;
+                break;
+            case 'b':
+                chessboard[i][j].p.type = Piece_type::Bishop;
+                break;
+            case 'q':
+                chessboard[i][j].p.type = Piece_type::Queen;
+                break;
+            case 'k':
+                chessboard[i][j].p.type = Piece_type::King;
+                break;
+            case 'p':
+                chessboard[i][j].p.type = Piece_type::Pawn;
+                break;
+            default:
+                chessboard[i][j].p.type = Piece_type::None;
+                chessboard[i][j].occupied = false;
             }
         }
     }
     // chessboard[0][0].p.type = Piece_type::Rook;
     // chessboard[0][0].p.colour = Colour::White;
     // chessboard[0][0].occupied = true;
-
 }
 
 Field_info &Board::get(Field f)
@@ -66,7 +65,7 @@ std::array<std::array<Field_info, 8>, 8> Board::get_state()
 {
     return chessboard;
 }
-bool checkCoord(int i) 
+bool checkCoord(int i)
 {
     return i >= 0 && i < 8;
 }
@@ -74,17 +73,16 @@ bool Board::make_move(Move move) // + check if legal
 {
     Field from = move.from;
     Field to = move.to;
-    if(!(checkCoord(from.row) && checkCoord(from.col) && checkCoord(to.row) && checkCoord(to.col))) //Wrong coords
+    if (!(checkCoord(from.row) && checkCoord(from.col) && checkCoord(to.row) && checkCoord(to.col))) // Wrong coords
     {
         return false;
     }
-
 
     if (!get(from).occupied) // Starting field is empty
     {
         return false;
     }
-    if (get(from).p.colour != colour_turn()) // Not ur turn b***
+    if (get(from).p.colour != colour_turn()) // Not ur turn
     {
         return false;
     }
@@ -93,18 +91,43 @@ bool Board::make_move(Move move) // + check if legal
         return false;
     }
 
+    bool is_move_legal;
     switch (get(from).p.type)
     {
-        case Piece_type::Rook:
-
-            break;
+    case Piece_type::Rook:
+        is_move_legal = check_rook_move(move.from, move.to);
+        break;
+    case Piece_type::Knight:
+        is_move_legal = check_knight_move(move.from, move.to);
+        break;
+    case Piece_type::Bishop:
+        is_move_legal = check_bishop_move(move.from, move.to);
+        break;
+    case Piece_type::Queen:
+        is_move_legal = check_queen_move(move.from, move.to);
+        break;
+    case Piece_type::King:
+        std::tuple<bool, bool, Move> kings_move = check_king_move(move.from,move.to);
+        is_move_legal = std::get<0>(kings_move);
+        break;
+    case Piece_type::Pawn:
+        is_move_legal = check_pawn_move(move.from,move.to);
+        break;
+    default:
+        is_move_legal = false;
     }
-    
+    if (!is_move_legal) // Checked if move is legal
+    {
+        return false;
+    }
+    // Empty "From" field, replace piece in "To" field
+    // Add move to moves, consider castling and pawn promotion (change piece type)
+    // Check whether King was attacked, end game if yes
+
 }
 Game_state Board::game_result()
 {
 }
-
 
 bool Board::check_rook_move(Field from, Field to)
 {
@@ -112,7 +135,7 @@ bool Board::check_rook_move(Field from, Field to)
     {
         for (int i = from.col + 1; i < to.col; i++)
         {
-            if (get(from.row,i).occupied)
+            if (get(from.row, i).occupied)
             {
                 return false;
             }
@@ -133,7 +156,6 @@ bool Board::check_rook_move(Field from, Field to)
         return false;
     }
     return true;
-    
 }
 
 bool check_knight_move(Field from, Field to)
@@ -160,14 +182,14 @@ bool Board::check_bishop_move(Field from, Field to)
     {
         return false;
     }
-    int delta_row = to.row - from.row > 0 ? 1: -1; // Check diagonal to move on
-    int delta_col = to.col - from.col > 0 ? 1: -1;
+    int delta_row = to.row - from.row > 0 ? 1 : -1; // Check diagonal to move on
+    int delta_col = to.col - from.col > 0 ? 1 : -1;
 
-    Field temp = 
-    {
-        from.row + delta_row,
-        from.col + delta_col,
-    };
+    Field temp =
+        {
+            from.row + delta_row,
+            from.col + delta_col,
+        };
     while (temp.row != to.row)
     {
         if (get(temp).occupied)
@@ -197,7 +219,7 @@ std::tuple<bool, bool, Move> Board::check_king_move(Field from, Field to) // 0 -
     {
         return {false, false, Move()};
     }
-    int starting_row = colour_turn() == Colour::White? 0 : 7; // Castling
+    int starting_row = colour_turn() == Colour::White ? 0 : 7; // Castling
     Field starting_king_field = {starting_row, 4};
 
     // for (auto x:history.get_moves()) // Check if king moved before
@@ -228,7 +250,7 @@ std::tuple<bool, bool, Move> Board::check_king_move(Field from, Field to) // 0 -
         king_direction = 1;
     }
 
-    for (auto x:history.get_moves()) // Check if king or rook moved before
+    for (auto x : history.get_moves()) // Check if king or rook moved before
     {
         if (x.from == starting_king_field)
         {
@@ -240,7 +262,7 @@ std::tuple<bool, bool, Move> Board::check_king_move(Field from, Field to) // 0 -
         }
     }
 
-    for(int i = from.col + king_direction; i != starting_rook_field.col; i += king_direction) // To be continued... or not, missing check if in check
+    for (int i = from.col + king_direction; i != starting_rook_field.col; i += king_direction) // To be continued... or not, missing check if in check
     {
         if (get(starting_row, i).occupied)
         {
@@ -273,7 +295,7 @@ bool Board::check_pawn_move(Field from, Field to)
     }
 
     bool has_moved = false;
-    for (auto x:history.get_moves()) // Check if pawn moved before
+    for (auto x : history.get_moves()) // Check if pawn moved before
     {
         if (x.from == from || x.to == from) // Nothing moved to or from this field
         {
@@ -281,7 +303,7 @@ bool Board::check_pawn_move(Field from, Field to)
             break;
         }
     }
-    if (!has_moved && to.row == from.row + 2*base_move_increment && to.col == from.col) // double move
+    if (!has_moved && to.row == from.row + 2 * base_move_increment && to.col == from.col) // double move
     {
         return true;
     }
@@ -292,7 +314,7 @@ bool Board::check_pawn_move(Field from, Field to)
     Move prev_move = history.get_back_element();
     Field prev_move_to = prev_move.to;
     Field prev_move_from = prev_move.from;
-    if (get(prev_move_to).p.type == Piece_type::Pawn &&  prev_move_to.row == prev_move_from.row + (-2)*base_move_increment) // prev move was dobule move pawn
+    if (get(prev_move_to).p.type == Piece_type::Pawn && prev_move_to.row == prev_move_from.row + (-2) * base_move_increment) // prev move was dobule move pawn
     {
         if (from.row == prev_move_to.row && abs(from.col - prev_move_to.col) == 1 && to.row == from.row + base_move_increment && to.col == prev_move_to.col) // en passaint capture
         {
@@ -302,5 +324,4 @@ bool Board::check_pawn_move(Field from, Field to)
     // TODO: PROMOTION
 
     return false;
-
 }
