342cae4708bfe123c2e7759093182a981c1bf86e
JakubWieclaw <jwieclaw000@gmail.com>
2023-01-17 22:07:12 +0100

    add fix in script compile

--- compile.sh
+++ compile.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rm chess.exe
 g++ -Iinclude -std=c++17 source/engine/Board.cpp source/engine/History.cpp source/front/console/Console_drawer.cpp source/front/console/Console_front.cpp source/front/console/Console_input.cpp source/front/console/File_input.cpp source/Field.cpp source/main.cpp -o console_chess.exe
